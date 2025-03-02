9d9a5d90d0b7c15e1ef409edaa5f9a01a9ab543d
Jahi Bennett <bennettjahi@gmail.com>
2025-03-02 18:27:17 -0500

    Remove some code that is no longer necessary

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
+from app import mail
 from flask import render_template, request, redirect, url_for, flash
-
+from flask_mail import Message
+from app.forms import ContactForm
 
 ###
 # Routing for your application.
@@ -17,12 +19,23 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data,
+        sender=(request.form['name'],request.form['email'] ),
+        recipients=["to@example.com"])
+        msg.body = request.form['message']
+        mail.send(msg)
+        flash('Your email was sent successfully!')
+        return redirect('/')   
+    """Render website's contact form page."""
+    return render_template('contact.html',form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
-
 # Flash errors from the form if validation fails
 def flash_errors(form):
     for field, errors in form.errors.items():
